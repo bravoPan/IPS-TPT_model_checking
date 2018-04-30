3823cd78cc57046929889bce0f86fbb9f7fc43fd
lilacqueen <38578892+listqueenstack@users.noreply.github.com>
2018-04-30 11:18:00 +0800

    update profile
    Text

---
+++ Wentao/ArrayStackTest.java
@@ -0,0 +1,8 @@
+package ArrayStack;
+import java.util.ArrayList;
+
+public class ArrayStackTest {
+    public  static void (String [] args){
+        ArrayStack<String> arrayStack = new ArrayStack<>();
+    }
+}
